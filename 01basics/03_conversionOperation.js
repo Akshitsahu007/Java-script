debfd4037552514fb15d06efec12af8ca8ddf9e2
Akshitsahu007 <102697199+Akshitsahu007@users.noreply.github.com>
2024-02-15 07:31:13 +0000

    new

---
+++ 01basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33";
+
+let valueInNumber = Number(score) ;
+
+//console.log (typeof score);
+//console.log (score);
+//console.log (typeof valueInNumber);
+//console.log (valueInNumber);
+
+// "33" =>  33 
+//"33avb" => NaN
+//true => 1; flase => 0
+
+let isLoggedIn =  true ;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false 
+//"" => false
+//"hitesh" => true
